b17869af02de36b025e3695f92ceaa4751f9ee2a
betzm <m.betzel@abeel.eu>
2021-12-07 12:17:37 +0100

    Refactored test folder

--- src/test/net/betzel/avro/schemagen/maven/plugin/test/Car.java
+++
@@ -1,7 +0,0 @@
-package net.betzel.avro.schemagen.maven.plugin.test;
-
-public abstract class Car {
-
-    public String color;
-
-}
--- src/test/net/betzel/avro/schemagen/maven/plugin/test/HotHatchback.java
+++
@@ -1,7 +0,0 @@
-package net.betzel.avro.schemagen.maven.plugin.test;
-
-public class HotHatchback extends Car {
-
-    public double horsepower;
-
-}
